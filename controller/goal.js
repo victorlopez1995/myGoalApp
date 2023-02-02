5ea116487ae7b06e7fbcde09073faa44ca250b7c
victorlopez1995 <lop22010@byui.edu>
2023-02-01 21:15:38 -0500

    correct get by id

--- controller/goal.js
+++ controller/goal.js
@@ -27,9 +27,6 @@ const getSingle = async (req, res, next) => {
       res.setHeader('Content-Type', 'application/json');
       res.status(200).json(lists[0]);  
     })
-    .catch(error => {
-      res.status(400).json({ message: err });
-      console.error(error)})
   };
 
   const createSingle = async (req, res, next) => {
--- controller/user.js
+++ controller/user.js
@@ -29,10 +29,7 @@ const getSingle = async (req, res, next) => {
     result.toArray().then((err,lists) => {
       res.setHeader('Content-Type', 'application/json');
       res.status(200).json(lists[0]);
-    })
-    .catch(error => {
-      res.status(400).json({ message: err });
-      console.error(error)});
+    });
   };
 
   const createSingle = async (req, res, next) => {
